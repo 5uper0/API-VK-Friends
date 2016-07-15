155efdcd4d6dd82fa5faa4d27f3bdce923fb1ae6
Супер Олег <go.super.0leg@gmail.com>
2016-07-15 14:18:02 +0300

    Initial Commit

---
+++ HomeWork45/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HomeWork45
+//
+//  Created by Oleh Veheria on 7/15/16.
+//  Copyright © 2016 Selfie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
